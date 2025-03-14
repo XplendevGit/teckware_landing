d4e4506917a45ab9e2c599fbb8569ac4bc75e221
XplendevGito <l.riveraa664@hotmail.com>
2025-03-14 02:49:07 -0300

    Updates V.0.10

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
     images: {
       domains: ['i.postimg.cc'], // Dominio de Postimage
     },
-    experimental: {
-        fontLoaders: [
-          { loader: '@next/font/google', options: { subsets: ['latin'] } },
-        ],
-      },
   };
   
   export default nextConfig;
