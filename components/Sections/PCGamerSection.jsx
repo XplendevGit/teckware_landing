718d2dc04959243821b5ac7dd395cf2fa5b1bdc1
XplendevGito <l.riveraa664@hotmail.com>
2025-03-29 05:14:11 -0300

    Updates V.0.12

--- components/Sections/PCGamerSection.jsx
+++ components/Sections/PCGamerSection.jsx
@@ -8,8 +8,8 @@ import Link from 'next/link';
 // Array de PCs Gamer
 const pcs = [
   {
-    image: 'https://i.postimg.cc/j2RYBrLH/prueba-a-Photoroom.png', // Imagen del PC
-    name: 'PC Gamer Model 1', // Nombre del PC
+    image: 'https://i.postimg.cc/8zYfyy73/Leviathan-1-Photoroom.png', // Imagen del PC
+    name: 'Leviathan', // Nombre del PC
     specs: [  // Especificaciones del PC
       '16GB RAM', 
       '1TB SSD', 
@@ -17,11 +17,11 @@ const pcs = [
       'MSI B450', 
       'Fuente de Poder 750W'
     ], 
-    link: '/productos/pc-gamer-modelo-1' // Enlace a la página o conversación de WhatsApp
+    link: 'https://wa.link/ebyzyu' // Enlace a la página o conversación de WhatsApp
   },
   {
-    image: 'https://i.postimg.cc/Fzw8N5KC/pcprueba-Photoroom.png', 
-    name: 'PC Gamer Model 2',
+    image: 'https://i.postimg.cc/D0hxSfFq/krakenv2-1-Photoroom.png', 
+    name: 'Kraken V2',
     specs: [
       '32GB RAM', 
       '2TB SSD', 
@@ -29,13 +29,13 @@ const pcs = [
       'ASUS Z490', 
       'Fuente de Poder 1000W'
     ], 
-    link: '/productos/pc-gamer-modelo-2'
+    link: 'https://wa.link/fdbgb2'
   },
 ];
 
 const PCGamerSection = () => {
   return (
-    <section className="bg-black py-10">
+    <section className="bg-[#111111] py-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.h2
           className="text-3xl font-bold text-center text-white mb-10"
@@ -56,32 +56,36 @@ const PCGamerSection = () => {
             {pcs.map((pc, index) => (
               <motion.div
                 key={index}
-                className="bg-white p-6 rounded-lg shadow-lg transform transition-all hover:scale-105"
+                className="p-6 rounded-lg shadow-lg transform transition-all"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.2 * index, duration: 0.5 }}
               >
-                <div className="flex items-center mb-4">
+                <div className="flex flex-col items-center mb-4 mx-auto">
                   {/* Imagen del PC */}
-                  <div className="w-1/3">
+                  <div className="w-full h-[300px] min-w-[300px] max-h-[300px] relative">
                     <Image
                       src={pc.image}
                       alt={pc.name}
-                      width={200} // Ajusta el tamaño según necesites
-                      height={200} // Ajusta el tamaño según necesites
-                      className="object-cover rounded-md"
+                      height={500}
+                      width={500}
+                      objectFit="cover"
+                      className="rounded-md mx-auto w-auto min-w-[300px] h-[300px] max-h-[300px]"
                     />
                   </div>
+
                   {/* Nombre del PC y características */}
-                  <div className="ml-4 w-2/3">
-                    <h3 className="text-xl font-semibold text-black mb-2">{pc.name}</h3>
-                    <ul className="text-gray-600 text-sm mb-4">
+                  <div className="w-full text-center md:pt-0">
+                    <h3 className="text-xl font-semibold text-[#9B4DE2] mb-2">{pc.name}</h3>
+                    <ul className="text-white text-sm mb-4">
                       {pc.specs.map((spec, specIndex) => (
                         <li key={specIndex} className="mb-1">{spec}</li>
                       ))}
                     </ul>
+
+                    {/* Botón de "Conocer más" */}
                     <Link href={pc.link}>
-                      <p className="block text-center px-6 py-2 bg-purple-600 text-white font-bold rounded-full hover:bg-purple-700 transition-all">
+                      <p className="block text-center px-6 py-2 bg-purple-600 text-white font-bold rounded-full hover:bg-purple-700 transition-all hover:scale-105">
                         Conocer más
                       </p>
                     </Link>
